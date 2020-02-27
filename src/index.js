e4b7f682c50ebd6e1036d156bcb27bf99e3828e6
Stan <mr.stanwong@gmail.comm>
2020-02-27 09:16:28 -0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,115 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            newItem: "",
+            list: []
+        };
+    }
+    componentDidMount() {
+        this.hydrateStateWithLocalStorage();
+        window.addEventListener(
+            "beforeunload",
+            this.saveStateToLocalStorage.bind(this)
+        );
+    }
+    componentWillUnmount() {
+        window.removeEventListener(
+            "beforeunload",
+            this.saveStateToLocalStorage.bind(this)
+        );
+        this.saveStateToLocalStorage();
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    hydrateStateWithLocalStorage() {
+        for (let key in this.state) {
+            if (localStorage.hasOwnProperty(key)) {
+                let value = localStorage.getItem(key);
+                try {
+                    value = JSON.parse(value);
+                    this.setState({ [key]: value });
+                } catch (e) {
+                    this.setState({ [key]: value });
+                }
+            }
+        }
+    }
+
+    saveStateToLocalStorage() {
+        for (let key in this.state) {
+            localStorage.setItem(key, JSON.stringify(this.state[key]));
+        }
+    }
+
+    updateInput(key, value) {
+        this.setState({ [key]: value });
+    }
+
+    addItem() {
+        const newItem = {
+            id: 1 + Math.random(),
+            value: this.state.newItem.slice()
+        };
+        const list = [...this.state.list];
+
+        list.push(newItem);
+
+        this.setState({
+            list,
+            newItem: ""
+        });
+    }
+
+    deleteItem(id) {
+        const list = [...this.state.list];
+        const updatedList = list.filter(item => item.id !== id);
+        this.setState({ list: updatedList });
+    }
+    render() {
+        return (
+            <div>
+                <h1>SpaceRexx</h1>
+                <h3 className="app-title"> Packing List</h3>
+
+                <div className="container">
+                    <div>
+                    
+                        Add an Item:
+          <br />
+                        <input
+                            type="text"
+                            placeholder="It's a long trip..."
+                            value={this.state.newItem}
+                            onChange={e => this.updateInput("newItem", e.target.value)}
+                        />
+                        <button
+                            className="add-btn btn-flat"
+                            onClick={() => this.addItem()}
+                            disabled={!this.state.newItem.length}
+                        >
+                            <i>Ruff! </i>
+                        </button>
+                        <br /> <br />
+                        <ul>
+                            {this.state.list.map(item => {
+                                return (
+                                    <li key={item.id}>
+                                        {item.value}
+                                        <button onClick={() => this.deleteItem(item.id)}>
+                                        [x]
+                                        </button>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+ReactDOM.render(<App />, document.getElementById('root'))
