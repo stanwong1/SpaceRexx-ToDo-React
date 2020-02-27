b3aba2460753d57ba3b0425a4c675b0a39ca2bba
Stan <mr.stanwong@gmail.comm>
2020-02-27 09:23:17 -0800

    Updates

---
+++ precache-manifest.626d09f1bfa6afbfb7c2aabb77076165.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a2199a464e43225f52397f7895042a4",
+    "url": "/SpaceRexx-ToDo-React/index.html"
+  },
+  {
+    "revision": "10df66d12026e7314b3b",
+    "url": "/SpaceRexx-ToDo-React/static/css/main.af718a96.chunk.css"
+  },
+  {
+    "revision": "68d21b78979aa9a983c2",
+    "url": "/SpaceRexx-ToDo-React/static/js/2.465f0ee4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/SpaceRexx-ToDo-React/static/js/2.465f0ee4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10df66d12026e7314b3b",
+    "url": "/SpaceRexx-ToDo-React/static/js/main.09642f36.chunk.js"
+  },
+  {
+    "revision": "bb63b10d580bbb95df21",
+    "url": "/SpaceRexx-ToDo-React/static/js/runtime-main.3cd95fb3.js"
+  }
+]);
